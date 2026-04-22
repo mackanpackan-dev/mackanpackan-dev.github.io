6fd37dd1aca22cd810ddba3158d8c0e54c287381
MackanPackan <markus.skuthalla@campusnykoping.se>
2026-04-22 09:29:12 +0200

    Initial push

---
+++ data.js
@@ -0,0 +1,50 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-kpno-drone-360-2-cc2",
+      "name": "KPNO-Drone-360-2-CC2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        },
+        {
+          "tileSize": 512,
+          "size": 8192
+        }
+      ],
+      "faceSize": 5319.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
