bf906f7d16ac4497fdb3ec8f77a23a121e4d43c1
MackanPackan <markus.skuthalla@campusnykoping.se>
2026-04-22 09:34:19 +0200

    La till punkter

--- data.js
+++ data.js
@@ -32,12 +32,37 @@ var APP_DATA = {
       ],
       "faceSize": 5319.5,
       "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
+        "yaw": 0.7321245916167989,
+        "pitch": 0.21874057505312905,
+        "fov": 1.4074099239952194
       },
       "linkHotspots": [],
-      "infoHotspots": []
+      "infoHotspots": [
+        {
+          "yaw": -0.0032954615059477277,
+          "pitch": -0.042678042856765686,
+          "title": "Högskolecentrums nya forskningslabb!",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.4342486236093581,
+          "pitch": 0.24922077210738536,
+          "title": "Lärarrum K81973B",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.7576960283212806,
+          "pitch": 0.0064082831937071205,
+          "title": "SFI-korridoren",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.8454343838923855,
+          "pitch": 0.045726360959941204,
+          "title": "Rutschkana 1",
+          "text": "Text"
+        }
+      ]
     }
   ],
   "name": "Project Title",
