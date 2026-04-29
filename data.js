a690f0294166318a88b5bf1e906129846ef13f0d
mackanpackan-dev <markus.skuthalla@campusnykoping.se>
2026-04-29 15:26:34 +0200

    Changed images

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-exempel-1",
-      "name": "Exempel 1",
+      "id": "0-1-120mp",
+      "name": "1. 120MP",
       "levels": [
         {
           "tileSize": 256,
@@ -20,58 +20,24 @@ var APP_DATA = {
         {
           "tileSize": 512,
           "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
         }
       ],
-      "faceSize": 2048,
+      "faceSize": 3880,
       "initialViewParameters": {
-        "yaw": 0.8790880150639282,
-        "pitch": 0.22044833989087032,
+        "yaw": -1.6190671244765404,
+        "pitch": 0.028168140056507696,
         "fov": 1.4074099239952194
       },
-      "linkHotspots": [
-        {
-          "yaw": 2.7303043368215043,
-          "pitch": 0.6455707035580591,
-          "rotation": 0,
-          "target": "2-exempel-3"
-        },
-        {
-          "yaw": -2.4481372398413157,
-          "pitch": -0.07131673387777582,
-          "rotation": 0,
-          "target": "1-exempel-2"
-        }
-      ],
-      "infoHotspots": [
-        {
-          "yaw": 2.715638532330953,
-          "pitch": 0.7685836422119827,
-          "title": "Innergården",
-          "text": "Text"
-        },
-        {
-          "yaw": 3.1061278392810454,
-          "pitch": 0.26388737521942396,
-          "title": "Västra klockstapeln",
-          "text": "Text"
-        },
-        {
-          "yaw": 3.1333771229260954,
-          "pitch": 0.6792751451912142,
-          "title": "Huvudentrén",
-          "text": "Text"
-        },
-        {
-          "yaw": -2.447351425720111,
-          "pitch": 0.36220776588904613,
-          "title": "Culturum",
-          "text": "Text"
-        }
-      ]
+      "linkHotspots": [],
+      "infoHotspots": []
     },
     {
-      "id": "1-exempel-2",
-      "name": "Exempel 2",
+      "id": "1-1-40mp",
+      "name": "1. 40MP",
       "levels": [
         {
           "tileSize": 256,
@@ -91,44 +57,53 @@ var APP_DATA = {
           "size": 2048
         }
       ],
-      "faceSize": 2048,
+      "faceSize": 1944,
       "initialViewParameters": {
-        "yaw": 0.183291584465044,
-        "pitch": 0.6244383781186347,
+        "yaw": -1.605511817391136,
+        "pitch": 0.048486189187915585,
         "fov": 1.4074099239952194
       },
-      "linkHotspots": [
-        {
-          "yaw": -1.291459282037776,
-          "pitch": -0.023182783620528724,
-          "rotation": 0,
-          "target": "0-exempel-1"
-        }
-      ],
-      "infoHotspots": [
-        {
-          "yaw": 0.0224486240194679,
-          "pitch": 0.9740082556421754,
-          "title": "Huvudentrén",
-          "text": "Text"
-        },
-        {
-          "yaw": 1.9676170686493464,
-          "pitch": 0.6173625106461493,
-          "title": "Culturum",
-          "text": "Text"
-        },
-        {
-          "yaw": 2.0226203188907395,
-          "pitch": 0.006453632277695576,
-          "title": "Vattentornet",
-          "text": "Text"
-        }
-      ]
+      "linkHotspots": [],
+      "infoHotspots": []
     },
     {
-      "id": "2-exempel-3",
-      "name": "Exempel 3",
+      "id": "2-2-120mp",
+      "name": "2. 120MP",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-2-40mp",
+      "name": "2. 40MP",
       "levels": [
         {
           "tileSize": 256,
@@ -148,46 +123,350 @@ var APP_DATA = {
           "size": 2048
         }
       ],
-      "faceSize": 2048,
+      "faceSize": 1944,
       "initialViewParameters": {
-        "yaw": 0.4672324742924516,
-        "pitch": 0.3251451483511225,
-        "fov": 1.4074099239952194
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
       },
-      "linkHotspots": [
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-3-120mp",
+      "name": "3. 120MP",
+      "levels": [
         {
-          "yaw": 1.5691703485943016,
-          "pitch": 0.16040223395501485,
-          "rotation": 0,
-          "target": "0-exempel-1"
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
         }
       ],
-      "infoHotspots": [
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-3-40mp",
+      "name": "3. 40MP",
+      "levels": [
         {
-          "yaw": 2.779569749308134,
-          "pitch": 0.38190121314214487,
-          "title": "Mackan",
-          "text": "Text"
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
         },
         {
-          "yaw": 0.4775764241917049,
-          "pitch": 0.325601883982074,
-          "title": "Åsa",
-          "text": "Text"
+          "tileSize": 512,
+          "size": 512
         },
         {
-          "yaw": -0.3975556535706346,
-          "pitch": 0.294785980546612,
-          "title": "Stefan",
-          "text": "Text"
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
         }
-      ]
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-4-120mp",
+      "name": "4. 120MP",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-4-40mp",
+      "name": "4. 40MP",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-5-120mp",
+      "name": "5. 120MP",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-5-40mp",
+      "name": "5. 40MP",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-6-120mp",
+      "name": "6. 120MP",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-6-40mp",
+      "name": "6. 40MP",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-7-120mp",
+      "name": "7. 120MP",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-7-40mp",
+      "name": "7. 40MP",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
     }
   ],
-  "name": "Project Title",
+  "name": "DJI Osmo 360-test",
   "settings": {
     "mouseViewMode": "drag",
-    "autorotateEnabled": true,
+    "autorotateEnabled": false,
     "fullscreenButton": true,
     "viewControlButtons": true
   }
